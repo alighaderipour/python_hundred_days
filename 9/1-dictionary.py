5db7124e9f4cdbc687da2f03452395e0b7f0d069
shahaz <ali.ghaderipour@gmail.com>
2024-06-23 11:05:52 +0330

    some practice to dict

--- 9/1-dictionary.py
+++ 9/1-dictionary.py
@@ -20,3 +20,43 @@ for item, value in  student_scores.items():
     if   91<=value<100    : 
         student_grades[item]= "Outstanding"
 print(student_grades)
+
+
+#! 👆 above code does not work 👆
+#! 👆 above code does not work 👆
+#! 👆 above code does not work 👆
+#! 👆 above code does not work 👆
+#! 👆 above code does not work 👆
+
+#?  👇 use blow code 👇
+#?  👇 use blow code 👇
+#?  👇 use blow code 👇
+#?  👇 use blow code 👇
+#?  👇 use blow code 👇
+
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+student_grades = {}
+
+    
+for item, value in  student_scores.items():
+    if value<=70 : 
+       grade= "Fail"
+    elif 71<=value<80    : 
+        grade= "Acceptable"
+    elif 81<=value<90    : 
+        grade= "Exceeds Expectations"
+    elif   91<=value<=100    : 
+        grade= "Outstanding"
+    else:
+        pass
+    student_grades[item] = grade
+
+
+print(student_grades)
