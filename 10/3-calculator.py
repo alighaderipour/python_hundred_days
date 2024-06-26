463ce92efe51e08187314b09c6578a75f5124f21
shahaz <ali.ghaderipour@gmail.com>
2024-06-26 13:36:13 +0330

    calc start

---
+++ 10/3-calculator.py
@@ -0,0 +1,22 @@
+def plus(a,b):
+    return a+b
+def divide(a,b):
+    return a/b
+def minus(a,b):
+    return a-b
+def multy(a,b):
+    return a*b
+
+operation ={}
+operation['+']  = plus
+operation['/']  = divide
+operation['-']  = minus
+operation['*']  = multy
+
+
+
+num1 = int(input('what is your first number :'))
+operat
+
+
+
