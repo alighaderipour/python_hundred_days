bc0bc67cc865ca641a8701c782a521535af35801
shahaz <ali.ghaderipour@gmail.com>
2024-06-27 10:00:48 +0330

    calc done day 11

--- 10/3-calculator.py
+++ 10/3-calculator.py
@@ -1,22 +1,37 @@
-def plus(a,b):
-    return a+b
-def divide(a,b):
-    return a/b
-def minus(a,b):
-    return a-b
-def multy(a,b):
-    return a*b
+def plus(a, b):
+    return a + b
 
-operation ={}
-operation['+']  = plus
-operation['/']  = divide
-operation['-']  = minus
-operation['*']  = multy
+def divide(a, b):
+    return a / b
 
+def minus(a, b):
+    return a - b
 
+def multy(a, b):
+    return a * b
 
-num1 = int(input('what is your first number :'))
-operat
+operationDict = {
+    '+': plus,
+    '/': divide,
+    '-': minus,
+    '*': multy
+}
 
+def calc(n1=None):
+    if n1 is None:
+        n1 = int(input('What is your first number: '))
+    operation = input('What operation would you like to choose? ')
+    n2 = int(input('What is your second number: '))
+    return operationDict[operation](n1, n2)
 
+answer = calc()
 
+while True:
+    user_continue = input(f"The result is {answer}! Would you like to continue with the result? Please choose 'yes' or 'no': ")
+    if user_continue.lower() == 'no':
+        print(f'Final answer is {answer}')
+        break
+    elif user_continue.lower() == 'yes':
+        answer = calc(answer)  # Update answer with the result of calc
+    else:
+        print('Invalid input')
