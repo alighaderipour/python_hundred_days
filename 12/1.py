101efae56d3cd8e0687ed3f0321954bc509968b1
shahaz <ali.ghaderipour@gmail.com>
2024-06-29 13:29:09 +0330

    nothing

--- 12/1.py
+++ 12/1.py
@@ -1,1 +1,13 @@
 ##scope##
+friends = 1
+
+# variable inside a function is local scope and cannot be accessed outside
+def increase_friends():
+    friends = 2
+    print('friends inside the function: ', friends)
+    
+increase_friends()
+print('enemies outside the function', friends)
+
+
+
