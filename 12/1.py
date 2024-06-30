1aab0e03f6a0402b8393b6e842b33280c81c7846
shahaz <ali.ghaderipour@gmail.com>
2024-06-30 13:44:58 +0330

    day 12

--- 12/1.py
+++ 12/1.py
@@ -2,6 +2,7 @@
 friends = 1
 
 # variable inside a function is local scope and cannot be accessed outside
+#? local scope
 def increase_friends():
     friends = 2
     print('friends inside the function: ', friends)
@@ -9,5 +10,21 @@ def increase_friends():
 increase_friends()
 print('enemies outside the function', friends)
 
+#? global scope
+player_health = 100
 
+def print_health():
+    player_str = 100
+    print(f'current player health :  {player_health}')
+
+print_health()
+
+
+#? modify global scope's value  ---> need "global keyword"
+def drink_potion():
+    global player_health
+    player_health -=10
+    print(f'drinking potion will decrease the player health, new health is {player_health}')
+
+drink_potion()
 
