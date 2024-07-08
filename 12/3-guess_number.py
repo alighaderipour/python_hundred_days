8ba2e92c6d3842233ea37304d48469eae5f48298
Shahaz <ali.ghaderipour@gmail.com>
2024-07-08 22:03:05 +0330

    start day 13

--- 12/3-guess_number.py
+++ 12/3-guess_number.py
@@ -5,14 +5,20 @@ print('target_number: ', target_number)
 
 
 def user_guess():
-    guessed_number = int(input('guess a number between 1 to 100 '))
+    global guess_count
+    if guess_count == 10 :
+        print(
+            f'you ran out of chances, the answer was {target_number}'
+        )
+        return True
+    guessed_number = int(input('guess a number between 1 to 100 , you have 10 try :  '))
     diff = target_number - guessed_number
     if abs(diff) == 0:
         print(f'you won!, the number was {guessed_number}')
         return True
         
     elif abs(diff) < 10:
-        global guess_count 
+         
         guess_count +=1
         if guessed_number < target_number:
             print('abit low, guess again!')
