daeab74d7038679a28e22f1ddafab4737fa727fa
shahaz <ali.ghaderipour@gmail.com>
2024-07-11 08:21:59 +0330

    day 14 project done, goign to day 15 intermediate

--- 13/debugging_solution.py
+++ 13/debugging_solution.py
@@ -1,17 +1,17 @@
 # ############DEBUGGING#####################
 
 # Describe Problem
-def my_function():
-  for i in range(1, 21):
-    if i == 20:
-      print("You got it")
-my_function()
+# def my_function():
+#   for i in range(1, 21):
+#     if i == 20:
+#       print("You got it")
+# my_function()
 
-# Reproduce the Bug
-from random import randint
-dice_imgs = ["1", "2", "3", "4", "5", "6"]
-dice_num = randint(0, 6)
-print(dice_imgs[dice_num])
+# # Reproduce the Bug
+# from random import randint
+# dice_imgs = ["1", "2", "3", "4", "5", "6"]
+# dice_num = randint(0, 6)
+# print(dice_imgs[dice_num])
 
 # # Play Computer
 # year = int(input("What's your year of birth?"))
---
+++ personal/random_choice.py
@@ -0,0 +1,5 @@
+import random
+listo = [1,2,5,6,7,8,567,45,2348,2]
+
+randomo = random.choice(listo)
+print('randomo: ', randomo)
