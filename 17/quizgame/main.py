36286918eb97f85c9726a098b8609668767e66da
shahaz <ali.ghaderipour@gmail.com>
2024-08-01 12:18:41 +0330

    day 17

--- 17/quizgame/main.py
+++ 17/quizgame/main.py
@@ -1,10 +1,12 @@
 from data import question_data
 from question_model import Question
+from quiz_brain import *
 
 question_bank = []
 for item in question_data:
     question = Question(item["text"], item["answer"])
     question_bank.append(question)
 
-for item in question_bank:
-    print(item.text, item.answer)
+
+q = QuizBrain(question_bank)
+q.next_question()
