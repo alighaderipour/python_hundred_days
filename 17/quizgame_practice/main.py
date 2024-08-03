ef3476882d0e7a3fab178057d5c4961ec93a58df
shahaz <ali.ghaderipour@gmail.com>
2024-08-03 10:44:14 +0330

    quizgame day 17 practice

---
+++ 17/quizgame_practice/main.py
@@ -0,0 +1,16 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import *
+
+question_bank = []
+for item in question_data:
+    question = Question(item["text"], item["answer"])
+    question_bank.append(question)
+
+
+q = QuizBrain(question_bank)
+print(q)
+
+
+while q.still_has_question():
+    q.next_question()
