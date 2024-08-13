d5f15553de5aec77a366d1a5e03a47f27fc74dd6
shahaz <ali.ghaderipour@gmail.com>
2024-08-13 13:18:07 +0330

    geometry game

--- Udemy_Intermediate_to_Advanced_Python_with_10_OOP_Projects/1- project one geometry game/1-all_elements_of_python.py
+++ Udemy_Intermediate_to_Advanced_Python_with_10_OOP_Projects/1- project one geometry game/1-all_elements_of_python.py
@@ -38,13 +38,25 @@ it's part of the memory|these are all objects because they
 occupy some space in memory ~> RAM so when C Python shell starts, these objects are 
 being created in RAM)
                                         --> ALL OBJECTS HAVE A TYPE  <--
+                                    --> OBJECTS ARE CREATED BY CLASSES <--
+                                    
+so when we say 5 is a class it's actually int(x=5) and it's from int class.
+when we say [5, 6] is an object it's actually list(5, 6) and it's from list class.
+
+
 2- Identifiers: a, b, c, d, area, x
 (identifiers are names that we use to identify an object for example "human being" is an object and "Ali" is an identifier)
+
 3- Operators : **
+
 4- Delimiters: , " () : []
 (https://docs.python.org/3/reference/lexical_analysis.html#operators)
+
 5- Keyword: def, return
+
 6- Comments : # all language elements in this code
+
 7- Blank Lines :
+
 8- White Spaces:
 """
