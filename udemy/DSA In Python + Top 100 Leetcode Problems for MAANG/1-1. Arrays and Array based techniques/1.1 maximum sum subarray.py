254c37ad49cd21ebf56bb0e94a5bdd00878bfb48
Shahaz <ali.ghaderipour@gmail.com>
2024-08-16 21:58:20 +0330

    leetcode first

--- udemy/DSA In Python + Top 100 Leetcode Problems for MAANG/1-1. Arrays and Array based techniques/1.1 maximum sum subarray.py
+++ udemy/DSA In Python + Top 100 Leetcode Problems for MAANG/1-1. Arrays and Array based techniques/1.1 maximum sum subarray.py
@@ -2,9 +2,9 @@ from faker import Faker
 
 faker = Faker()
 
-int_num = [faker.pyint(-10, 10) for _ in range(11)]
+int_num = [faker.pyint(-10, 10) for _ in range(3333)]
 print(int_num)
-# int_num =
+
 max_list = []
 x = float("-inf")
 max_list.append(x)
@@ -12,12 +12,9 @@ for item in range(len(int_num)):
     temp_sum = 0
     for item2 in range(item, len(int_num)):
         temp_sum += int_num[item2]
-        print(temp_sum)
-        print(max(max_list))
         if temp_sum >= max(max_list):
             max_list.append(temp_sum)
 
-        max_list.append(temp_sum)
 
 print(max_list)
 print(max(max_list))
