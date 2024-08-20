16b7c0543aa2014cc2031a950fc83f4d66a23cca
shahaz <ali.ghaderipour@gmail.com>
2024-08-20 18:30:44 +0330

    practice advance

--- udemy/DSA In Python + Top 100 Leetcode Problems for MAANG/1-1. Arrays and Array based techniques/2- buy and sell stock.py
+++ udemy/DSA In Python + Top 100 Leetcode Problems for MAANG/1-1. Arrays and Array based techniques/2- buy and sell stock.py
@@ -1,2 +1,14 @@
-z = [lambda x: x + 1 for x in range(10)]
-print(z[0](3))
+def processing_items(item_type, **kwargs):
+    print(f"processing {item_type} : ...")
+    for key, value in kwargs.items():
+        print(f"{key} : {value}")
+    print("processing done")
+
+
+processing_items(
+    "book", title="100 Years Solitude", Author="Gabriel GarciaMarquez", price="43$"
+)
+processing_items(
+    "book", title="Crime and Punishment", Author="Dastaiovski", price="41$"
+)
+processing_items("Ball", size="medium", sport="Voleyball", price="21$")
