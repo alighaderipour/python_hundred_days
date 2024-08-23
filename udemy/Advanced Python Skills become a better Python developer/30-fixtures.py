bb23dbbde2858b496d0d6146f211ad0b057a866a
Shahaz <ali.ghaderipour@gmail.com>
2024-08-23 19:32:52 +0330

    intermediate done

---
+++ udemy/Advanced Python Skills become a better Python developer/30-fixtures.py
@@ -0,0 +1,6 @@
+"""
+1- Test fixtures initializes test functions.
+2- They provide a fixed baseline so that tests execute reliably and produce consistent, repeatable resutls.
+3- Initialization may set up service, state or other operation environment.
+
+"""
