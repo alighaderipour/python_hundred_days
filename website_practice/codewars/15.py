cadeeb1b77b77d462616484f0da5a3e106d11642
Shahaz <ali.ghaderipour@gmail.com>
2024-09-19 16:05:21 +0330

    test

--- website_practice/codewars/15.py
+++ website_practice/codewars/15.py
@@ -2,3 +2,20 @@
 
 
 """
+
+
+def number(bus_stops):
+    return sum([stop[0] - stop[1] for stop in bus_stops])
+
+
+def number(stops):
+    return sum(i - o for i, o in stops)
+
+
+def number(arr):
+    people_in = 0
+    people_out = 0
+    for stops in arr:
+        people_in += stops[0]
+        people_out += stops[1]
+    return people_in - people_out
