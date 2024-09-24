03b9899f8ae39406f2bc40c85e9f3a8f91bb8940
shahaz <ali.ghaderipour@gmail.com>
2024-09-24 13:43:50 +0330

    14030703

--- CBTNuggets - Certified Professional in Python Programming 1 (PCPP1)/1. Understand Object-Oriented Programming Basics/7. Solution Walk-Through Build a Library Management System.py
+++ CBTNuggets - Certified Professional in Python Programming 1 (PCPP1)/1. Understand Object-Oriented Programming Basics/7. Solution Walk-Through Build a Library Management System.py
@@ -1,22 +1,46 @@
 class Book:
-    def __init__(self, title, author, num_pages):
+    def __init__(self, title, author, year, status):
         self.title = title
         self.author = author
-        self.num_pages = num_pages
+        self.year = year
+        self.status = status
+
+    # def __str__(self):
+    #     return f"'{self.title}' by {self.author} - Pages: {self.year}, Status: {self.status}"
+
+    def __repr__(self):
+        return f"'{self.title}' by {self.author} - Pages: {self.year}, Status: {self.status}"
 
 
 class Library:
     def __init__(self):
-        pass
+        self.books = []
 
-    def add_book(self, book):
-        pass
+    def add_book(self, book_name):
+        self.books.append(book_name)
 
-    def remove_book(self):
-        pass
+    def remove_book(self, title):
+        self.books = [book for book in self.books if book.title != title]
 
-    def check_in(self):
-        pass
+    def check_in(self, title):
+        book = next((book for book in self.books if book.title == title), None)
+        if book is not None:
+            book.status = "available"
 
-    def check_out(self):
-        pass
+    def check_out(self, title):
+        book = next((book for book in self.books if book.title == title), None)
+        if book is not None:
+            book.status = "borrowed"
+
+    def display_books(self):
+        for book in self.books:
+            print(book)
+
+
+the_alchemist = Book("Alchemist", "Paulo Coelho", 2008, "available")
+my_library = Library()
+my_library.add_book(the_alchemist)
+my_library.add_book(
+    Book(title="Blindness", author="José Saramago", year=1995, status="available")
+)
+my_library.display_books()
