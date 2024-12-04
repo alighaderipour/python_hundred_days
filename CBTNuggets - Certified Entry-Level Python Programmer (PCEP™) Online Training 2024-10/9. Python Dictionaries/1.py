177053196b43af4f644296aee400d13d2362e141
shahaz <ali.ghaderipour@gmail.com>
2024-12-04 11:17:22 +0330

    14030914

--- CBTNuggets - Certified Entry-Level Python Programmer (PCEP™) Online Training 2024-10/9. Python Dictionaries/1.py
+++ CBTNuggets - Certified Entry-Level Python Programmer (PCEP™) Online Training 2024-10/9. Python Dictionaries/1.py
@@ -1,13 +1,33 @@
+print("-------------------create  dictionary--------------------------")
 car_stock = {"Toyota": 10, "Nissan": 13, "Peugeot": 12}
+print(car_stock)
+print("-------------------get  dictionary value --------------------------")
 Toyota_val = car_stock["Toyota"]
 Nissan_val = car_stock.get("Nissan")
-print(Toyota_val, " , ", Nissan_val)
+print("Toyota Inventory : ", Toyota_val, "Toyota Inventory", Nissan_val)
 
 car_stock.update({"Volvo": 25})
 print(car_stock)
-
+print("-------------------all key,values --------------------------")
+print(car_stock.keys())
+print(car_stock.values())
+print(car_stock.items())
+print("------------------------------------------------------------")
 # when you want to create a dict out of a tuple with a single value for all keys
 club_names = ("Perspolis", "Mes", "Nassaji", "Zobahan")
 intial_value = 0
 club_val = dict.fromkeys(club_names, intial_value)
 print(club_val)
+print("------------------------------------------------------------")
+
+dict_names = {"Ali": 12, "Reza": 43, "Aria": 64, "Kourosh": 3, "Maryam": 26}
+dict_names.pop("Alia", print("the name you tried to pop was not found"))
+print("------------------------------------------------------------")
+from python_hundred_days.radnom_generators.random_dict import main
+
+dict1 = main(5)
+removed_item = dict1.popitem()
+print(dict1)
+print(removed_item)
+
+print("------------------------------------------------------------")
