a2817e7572fa16037f34701779ab00be5d65cbc0
shahaz <ali.ghaderipour@gmail.com>
2024-12-04 12:20:56 +0330

    14030914

--- CBTNuggets - Certified Entry-Level Python Programmer (PCEP™) Online Training 2024-10/9. Python Dictionaries/1.py
+++ CBTNuggets - Certified Entry-Level Python Programmer (PCEP™) Online Training 2024-10/9. Python Dictionaries/1.py
@@ -12,17 +12,20 @@ print("-------------------all key,values --------------------------")
 print(car_stock.keys())
 print(car_stock.values())
 print(car_stock.items())
-print("------------------------------------------------------------")
+print("----------------------fromkeys--------------------------------")
 # when you want to create a dict out of a tuple with a single value for all keys
 club_names = ("Perspolis", "Mes", "Nassaji", "Zobahan")
 intial_value = 0
 club_val = dict.fromkeys(club_names, intial_value)
 print(club_val)
-print("------------------------------------------------------------")
-
+print("------------------------pop-----------------------------")
+# dict.pop(key, default)
+# key: The key to remove from the dictionary.
+# default (optional): The value to return if the key is not found.
 dict_names = {"Ali": 12, "Reza": 43, "Aria": 64, "Kourosh": 3, "Maryam": 26}
 dict_names.pop("Alia", print("the name you tried to pop was not found"))
-print("------------------------------------------------------------")
+print("---------------------popitem-------------------------------")
+# The popitem() method in Python is used with dictionaries to remove and return the last key-value pair as a tuple. It follows LIFO (Last In, First Out) order for removal.
 from python_hundred_days.radnom_generators.random_dict import main
 
 dict1 = main(5)
@@ -30,4 +33,4 @@ removed_item = dict1.popitem()
 print(dict1)
 print(removed_item)
 
-print("------------------------------------------------------------")
+print("--------------------dictionary comprehension---------------------------")
