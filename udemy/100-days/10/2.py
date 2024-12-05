a14fee9e873c95ca061023f151ee202201508c42
Shahaz <ali.ghaderipour@gmail.com>
2024-12-05 09:33:20 +0330

    14030914

--- udemy/100-days/10/2.py
+++
@@ -1,8 +0,0 @@
-def titleNames(fname, lname):
-    
-    if fname=="" or lname=="":
-        print("empty input")
-    else:
-        print(fname.title() + ' ' + lname.title())
-
-titleNames('ALI', 'GhAderi pour')
