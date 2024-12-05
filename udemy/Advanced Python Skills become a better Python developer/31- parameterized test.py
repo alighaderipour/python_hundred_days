1badf75d4ad1db23d21fccab43bc655f32c1ee2c
Shahaz <ali.ghaderipour@gmail.com>
2024-12-05 09:33:20 +0330

    14030914

--- udemy/Advanced Python Skills become a better Python developer/31- parameterized test.py
+++
@@ -1,3 +0,0 @@
-"""
-1- The @pytest.mark.parameterize decorator allows us to pass several cases to one test.
-"""
--- udemy/Advanced Python Skills become a better Python developer/faker.py
+++
@@ -1,8 +0,0 @@
-from faker import Faker
-
-faker = Faker()
-
-students = [faker.name() for _ in range(5)]
-grades = [faker.pyfloat(2, 2, True, 0.01, 20) for _ in range(5)]
-for index, (student, grade) in enumerate(zip(students, grades)):
-    print(f"{index} - {student}: {grade}")
