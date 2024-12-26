030757fe349cd65d5e09f90c49011f268b0e025a
shahaz <ali.ghaderipour@gmail.com>
2024-12-26 10:46:31 +0330

    test

--- test/test2.py
+++
@@ -1,9 +0,0 @@
-from faker import Faker
-
-fake = Faker()
-fake_list = [fake.pyint(min_value=10, max_value=20) for i in range(200)]
-
-print(fake_list)
-
-counter = {i: fake_list.count(i) for i in set(fake_list)}
-print(counter)
